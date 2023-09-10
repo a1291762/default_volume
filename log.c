64a80ff7e694dba163c13472b0d85d14280f9d47
Lincoln Ramsay <a1291762@gmail.com>
2023-09-10 17:19:34 +1000

    write logs to debugview and console
    The latter is only useful when compiled without -mwindows

--- log.c
+++ log.c
@@ -2,19 +2,25 @@
 #include <strsafe.h>
 
 void dbglog(const char *format, ...) {
+	char buffer[10000];
 	va_list args;
 	va_start(args, format);
-	char buffer[10000];
 	StringCbVPrintfA(buffer, sizeof(buffer), format, args);
-	OutputDebugStringA(buffer);
 	va_end(args);
+	// DebugView
+	OutputDebugStringA(buffer);
+	// console (if compiled without -mwindows)
+	printf("%s", buffer);
 }
 
 void wdbglog(const TCHAR *format, ...) {
+	TCHAR buffer[10000];
 	va_list args;
 	va_start(args, format);
-	TCHAR buffer[10000];
 	StringCbVPrintf(buffer, sizeof(buffer), format, args);
-	OutputDebugString(buffer);
 	va_end(args);
+	// DebugView
+	OutputDebugString(buffer);
+	// console (if compiled without -mwindows)
+	wprintf(L"%ls", buffer);
 }
